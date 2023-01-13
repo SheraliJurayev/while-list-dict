b47bf6093fb17c23bde170efdfd66dd1782fc8e3
Sherali Jurayev <100849791+SheraliJurayev@users.noreply.github.com>
2023-01-14 04:10:25 +0500

    exercises dict-list-while

---
+++ while-list-dict.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 14 00:44:51 2023
+
+@author: Sh_Jurayeff
+"""
+
+#print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+#ismlar = []
+#n=1
+#while True:
+#    savol = f"{n} - do'stingizni ismoni kiriting :  "
+#    ism = input(savol)
+#    ismlar.append(ism)
+#    takrorlash=input("Yana ismlar qo'shasizmi? (ha/yo'q)")
+#    n+=1
+#    if takrorlash !='ha':
+#        break
+
+#print("Dostlaringiz yoshini saqlaymiz")
+#dostlar={}
+#ishora = True
+#while ishora:
+#    ism=input ("Do'stingiz ismini kiriting: ")
+#    yosh=input(f"{ism.title()}ning yoshini kiriting:")
+#    dostlar[ism]=int(yosh)
+#    
+#    javob=input("Yana ma'lumot qo'shasizmi? ha/yo'q ")
+#    if javob == "yo'q":
+#        ishora=False
+
+#for ism,yosh in dostlar.items():
+#    print(f"{ism.title()} {yosh} yoshda")
+
+
+#print("Davlatlar poytaxtlarini saqlaymiz")
+#davlat={}
+#ishora=True
+#while ishora:
+#    nomi=input("Davlat nomini kiriting: ")
+#    poytaxt=input(f"{nomi.title()} ning  poytaxtini kiriting: ")
+#    davlat[nomi]=(poytaxt)
+#    
+#    sorov= input("Yana ma'lumot qo'shasizmi? ha/yuq ")
+#    if sorov == 'yuq':
+#        ishora=False
+#        
+#for nomi,poytaxt in davlat.items():
+#    print(f"{nomi.title()} ning poytaxti {poytaxt}")
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
